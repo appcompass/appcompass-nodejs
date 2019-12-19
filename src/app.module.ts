0bd42ec6d80e78476e9ba8f2b7a67ffb62aab50d
Jubair Saidi <jubair.saidi@gmail.com>
2019-12-18 23:42:42 -0500

    Adds initial structure for auth and user handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,17 @@ import { ConfigService } from './config/config.service';
 import { DBConfigService } from './db/db-config.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule,
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useClass: DBConfigService
-    })
+    }),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService]
